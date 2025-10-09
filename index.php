87813fac5c16a70dab700dd103a17c53f0de7338
KingJamesGOAT <sbstevebenjamin@gmail.com>
2025-10-09 21:10:40 +0200

    Update index.php
    Add SQL query to select all tasks from todo tabl

--- index.php
+++ index.php
@@ -80,7 +80,8 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+// Select all tasks
+$selectQuery = "SELECT * FROM todo ORDER BY created_at DESC";
 $items = $db->query($selectQuery);
 ?>
 
